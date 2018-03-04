20cf44714e7650ffdf3b5ac47a5039c344ac4464
Ramya <ramyakrc10@gmail.com>
2018-03-03 20:50:47 -0600

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Rectarea
+    {
+        static void Main()
+        {
+            Console.Write("Enter the length:");
+            int length = int.Parse(Console.ReadLine());
+            Console.Write("Enter the breadth:");
+            int breadth = int.Parse(Console.ReadLine());
+            Console.Write("the area is: {0}", length * breadth);
+            Console.Read();
+        }
+    }
+}
